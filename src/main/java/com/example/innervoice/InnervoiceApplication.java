90af019be386f40e1a20b72d707868a10cebf164
maark1106 <mark1106@naver.com>
2025-04-21 21:15:35 +0900

    git init

---
+++ src/main/java/com/example/innervoice/InnervoiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.innervoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnervoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnervoiceApplication.class, args);
+	}
+
+}
