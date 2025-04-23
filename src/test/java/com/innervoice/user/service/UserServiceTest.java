252fc1515a7ca8e7ae926cef895a65217b49b993
maark1106 <mark1106@naver.com>
2025-04-23 16:35:21 +0900

    test: login service 테스트 코드 작성(#3)

--- src/test/java/com/innervoice/user/service/UserServiceTest.java
+++ src/test/java/com/innervoice/user/service/UserServiceTest.java
@@ -3,8 +3,11 @@ package com.innervoice.user.service;
 import com.innervoice.user.domain.Role;
 import com.innervoice.user.domain.User;
 import com.innervoice.user.dto.request.UserCreateRequest;
+import com.innervoice.user.dto.request.UserLoginRequest;
 import com.innervoice.user.dto.response.UserCreateResponse;
+import com.innervoice.user.dto.response.UserLoginResponse;
 import com.innervoice.user.exception.UserException;
+import com.innervoice.user.exception.UserExceptionType;
 import com.innervoice.user.repository.UserRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -105,5 +108,56 @@ class UserServiceTest {
         //then
         verify(userRepository, times(1)).findByName("joon");
         verify(userRepository, times(1)).save(any(User.class));
+    }
+
+    @Test
+    @DisplayName("login 성공 시 UserLongResponse를 반환한다")
+    void login_success_returns_response(){
+        //given
+        UserLoginRequest request = new UserLoginRequest("joon", "password1");
+        User user = mock(User.class);
+        given(user.getPassword()).willReturn("password1");
+        given(user.getId()).willReturn(1L);
+        given(user.getRole()).willReturn(Role.PARENT);
+        given(user.getName()).willReturn(request.name());
+        given(userRepository.findByName("joon")).willReturn(Optional.of(user));
+
+        //when
+        UserLoginResponse response = userService.login(request);
+
+        //then
+        assertThat(response.id()).isEqualTo(1L);
+        assertThat(response.name()).isEqualTo("joon");
+    }
+
+    @Test
+    @DisplayName("login 시 존재하지 않는 사용자면 USER_NOT_FOUND 예외 발생")
+    void login_user_not_found_throws_exception(){
+        //given
+        UserLoginRequest request = new UserLoginRequest("unknown", "password1");
+
+        //when && then
+        assertThatThrownBy(() -> userService.login(request))
+                .isInstanceOf(UserException.class)
+                .extracting("exceptionType")
+                .isEqualTo(UserExceptionType.USER_NOT_FOUND);
+    }
+
+    @Test
+    @DisplayName("비밀번호 불일치 시 INVALID_PASSWORD 예외 발생")
+    void login_invalid_password_throws_exception() {
+        // given
+        UserLoginRequest request = new UserLoginRequest("joon", "wrong-password");
+
+        User user = mock(User.class);
+        given(user.getPassword()).willReturn("password1");
+        given(userRepository.findByName(request.name()))
+                .willReturn(Optional.of(user));
+
+        // when & then
+        assertThatThrownBy(() -> userService.login(request))
+                .isInstanceOf(UserException.class)
+                .extracting("exceptionType")
+                .isEqualTo(UserExceptionType.INVALID_PASSWORD);
     }
 }
