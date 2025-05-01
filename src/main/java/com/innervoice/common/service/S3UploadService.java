de2f455a99eeb6a5718f8ea670a9b0e64c117a52
maark1106 <mark1106@naver.com>
2025-05-01 15:23:43 +0900

    feat: S3 이미지 업로드 로직 추가 (#7)

---
+++ src/main/java/com/innervoice/common/service/S3UploadService.java
@@ -0,0 +1,71 @@
+package com.innervoice.common.service;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.innervoice.common.exception.CommonException;
+import com.innervoice.common.exception.CommonExceptionType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+public class S3UploadService {
+
+    private final AmazonS3Client amazonS3;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucketName;
+
+    @Value("${cloud.aws.s3.url}")
+    private String defaultUrl;
+
+    public String uploadFile(String dir, MultipartFile file) {
+        try {
+            String bucketDir = bucketName + dir;
+            String dirUrl = defaultUrl + dir + "/";
+            String fileName = generateFileName(file);
+            amazonS3.putObject(bucketDir, fileName, file.getInputStream(), getObjectMetadata(file));
+            return dirUrl + fileName;
+        } catch (IOException e) {
+            throw new CommonException(CommonExceptionType.FILE_UPLOAD_ERROR);
+        }
+    }
+
+    private ObjectMetadata getObjectMetadata(MultipartFile file) {
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentType(file.getContentType());
+        objectMetadata.setContentDisposition("inline");
+        objectMetadata.setContentLength(file.getSize());
+        return objectMetadata;
+    }
+
+    private String generateFileName(MultipartFile file) {
+        String extension = getFileExtension(file.getOriginalFilename());
+        return UUID.randomUUID().toString() + (extension.isEmpty() ? "" : "." + extension);
+    }
+
+    private String getFileExtension(String originalName) {
+        if (originalName == null) {
+            return "";
+        }
+        int dotIndex = originalName.lastIndexOf('.');
+        if (dotIndex == -1 || dotIndex == originalName.length() - 1) {
+            return "";
+        }
+        return originalName.substring(dotIndex + 1).toLowerCase();
+    }
+
+    public void deleteFile(String dir, String fileUrl) {
+        String fileName = extractFileName(fileUrl);
+        amazonS3.deleteObject(bucketName + dir, fileName);
+    }
+
+    private String extractFileName(String fileUrl) {
+        return fileUrl.substring(fileUrl.lastIndexOf("/") + 1);
+    }
+}
