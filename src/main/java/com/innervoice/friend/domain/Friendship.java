a748409c1dc6267d67b81cf5b8081efb7d1fd128
maark1106 <mark1106@naver.com>
2025-05-01 12:02:48 +0900

    feat: FriendShip 도메인 생성(#5)

---
+++ src/main/java/com/innervoice/friend/domain/Friendship.java
@@ -0,0 +1,33 @@
+package com.innervoice.friend.domain;
+
+import com.innervoice.user.domain.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "friendships")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Friendship {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "friend_id", nullable = false)
+    private User friend;
+
+    @Builder
+    public Friendship(User user, User friend) {
+        this.user = user;
+        this.friend = friend;
+    }
+}
